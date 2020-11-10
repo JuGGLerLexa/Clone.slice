03c4a2723035af0a52f0ddc4e07f6dc9c20189cd
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2020-11-10 21:26:43 +0200

    Initial commit

---
+++ JSDZ1/JS1dz.js
@@ -0,0 +1,436 @@
+// 1 - присвоить каждому из следующих значений свою переменную:
+//     'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+// Вывести каждую при помощи console.log , alert, document.write
+//
+// let hello;
+// hello = 'Hello';
+// console.log(hello);
+// // alert(hello);
+// // document.write(hello);
+//
+// let owu;
+// owu = 'okten';
+// console.log(owu);
+// // alert(owu);
+// // document.write(owu);
+//
+//
+// let com;
+// com = 'ua';
+// console.log(com);
+// // alert(com);
+// // document.write (com);
+//
+// let ua;
+// ua = '123';
+// console.log(ua);
+//
+//
+// let one;
+// one = '1';
+// // alert(one);
+// console.log(one);
+//
+// let ten;
+// ten = '10';
+// console.log(ten);
+//
+// let number;
+// number = -999;
+// console.log(number);
+//
+// let num;
+// num = 123;
+// console.log(num);
+//
+// let numb;
+// numb = 3.14;
+// // alert(numb);
+// console.log(numb);
+//
+// let numbe;
+// numbe = 2.7;
+// // alert(numbe);
+// console.log(numbe);
+//
+// let numsix;
+// numsix = 16;
+// // alert(numsix);
+// console.log(numsix);
+//
+// let tru;
+// tru = true;
+// console.log(tru);
+//
+//
+// // 2- переопределить каждую переменную из задания 1 дав им произвольные значения
+// // Вывести каждую при помощи console.log , alert, document.write
+//
+// hello = 'World';
+// console.log(hello);
+//
+// owu = 'Web';
+// console.log(owu);
+//
+// com = 'com';
+//
+// ua = 'ru';
+//
+// one = 'two';
+//
+// num = 321;
+// console.log(num);
+//
+// tru = false;
+// console.log(tru);
+//
+// // 3 - Создать 3 числовых и 3 стринговых константы. Вывести каждую при помощи console.log , alert, document.write
+//
+// const first = 1;
+// const second = 2;
+// const next = 3;
+// const str = "owu";
+// const str1 = "okten";
+// const str2 = '4';
+// console.log(first);
+// alert(next);
+// document.write(str2);
+
+
+// 4 - при помощи 3х разных prompt() получить 3 слова которые являются вашими ФИО.
+// Для фамилии имени и отчества создать разные переменные.
+// Вывести каждую при помощи console.log , alert, document.write
+//
+// let lastName = prompt ('Klypach');
+// let name = prompt ('Oleksii');
+// let surName = prompt ('Andreevich');
+// // alert(lastName + name + surName);
+// // alert(name);
+//
+// console.log(`${lastName} ${name} ${surName}`)
+
+
+
+
+// 5 - Взять переменные из задания 4 и сконкатенировать их в одной переменной person.
+
+
+// let person = (`${lastName} ${name} ${surName}`);
+//
+//
+//
+// // 6 - Взять задние 4 и 5 и применить его к ФИО всех членов своей семьи.
+//
+// let nameMather = prompt ('Tetyana');
+// let surMather = prompt('Volodymurivna');
+// let lastNMather = prompt('Klypach')
+// console.log(`${nameMather} ${surMather} ${lastNMather}`);
+//
+// let personM = (`${nameMather} ${surMather} ${lastNMather}`);
+
+// 7 - при помощи prompt() получить 3 числа. Привести их к числовому типу при помощи +. вывести их в консоль.
+
+// let number1 = +prompt('number 1');
+// let number2 = +prompt("number 2");
+// let number3 = +prompt("number 3");
+// console.log(number1, number2, number3)
+//
+// let numberOne = prompt(5 + 10);
+// let numberTwo = prompt(15+20);
+// let numberThree = prompt(35+40);
+// console.log(numberOne);
+// console.log(numberOne + numberTwo);
+// console.log(numberOne + numberTwo + numberThree);
+
+
+
+
+
+
+// 8 - при помощи prompt() получить 4 числа. Привести их к числовому типу при помощи parseInt.
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+//
+// console.log(number1);
+// let qwe = parseInt(number1);
+// console.log(number1)
+// let number2 = parseInt(prompt('numb'));
+// console.log(number2);
+
+
+
+// 9 - при помощи prompt()  получить 3 числа с плавающей точекой.
+// при помощи parseFloat привести их к соответсвующему типу.
+// Сложить их между собой записав результат в переменную result и вывести в консоль браузера
+
+
+
+
+
+
+
+
+
+// 10 - при помощи prompt()  получить 3 числа с плавающей точекой.
+// Округлить их при помощи Math.round Сложить их между собой
+// записав результат в переменную  и вывести в консоль браузера
+
+
+
+
+
+
+
+
+
+
+
+// 11 - при помощи prompt()  получить 2 целых числа. Привести их к целочисленному типу.
+// Первое число - это число которое будут возводить в степень.
+// Второе число - это число которое является степенью.
+// При помощи Math.pow возвести первое число в степень второго числа.
+
+
+
+
+
+
+// 12 - При помощи оператора опредеоения типа typeof определить типы следующих переменных и вывести их
+// в консоль let a = 100; let b = '100'; let c = true; let d = undefined;
+
+
+
+
+
+// 13 - Поставьте соответствующий оператор в выражениях что бы получился соответсвующий результат.
+
+
+
+
+
+
+
+
+// 14 - В одинаковых выражениях в задании не использовать два одинаковых оператора.
+
+
+
+
+
+
+
+
+
+// 15 -  Каждое выражение вложить в свою переменную
+// DONE
+
+//let taskA = (5 < 6)// -> true
+//console.log(taskA);
+
+// let taskB = (5 > 6); // -> false
+// console.log(taskB);
+//
+// let taskC = (5 >= 6); // -> false
+// console.log(taskC);
+//
+// let taskD = (5 === 6); // -> false
+// console.log(taskD);
+
+// let taskE = (10 == 10); // -> true
+// console.log(taskE);
+
+// let taskF = (10 == 10); // -> true
+// console.log(taskF);
+//
+// let taskG = (10 > 10); // -> false
+// console.log(taskG);
+//
+// let taskI = (10 < 10 ); // -> false
+// console.log(taskI);
+//
+// let taskJ = (10 != 10); // -> false
+// console.log(taskJ);
+//
+// let taskL = (123 === '123'); // -> false
+// console.log(taskL);
+//
+// const taskM = (123 == '123'); // -> true
+// console.log(taskM);
+
+
+//   CLASS
+// 1. Три різних числа вводяться через prompt().
+//     За допомоги if else вивести іх в порядку зростання. (відсортувати по зростанню)
+//
+
+// 2.
+// Все параматры получаем с клавиатуры!!!!
+//     Имитируем поведение пешехода на перекстке.
+//     Если светофор зеленый - вывести "иди".
+//     Если светофор желтый - вывести "подожди".
+//     Если светофор красный - вывести "стой".
+//     Если светофор в аварийном режиме вывести "делай что хочешь"!
+//
+//     3
+// Все параметры получаем с клавиатуры!!!!(prompt , confirm)
+// Создать переменную isRoadClear которая характеризирует наличие на дороге машин.
+//     Улучшаем предыдущее задание.
+//     Если светофор зеленый и машин нет - вывести "иди".
+//     Если светофор зеленый и машины есть  - вывести подожди пока нарушители проедут".
+// Если светофор желтый и машины есть - вывести "жди".
+//     Если светофор желтый и машин нет - вывести "все рано жди".
+//     Если светофор красный и машин нет- вывести "стой все рано".
+//     Если светофор красный - и машины есть вывести "стой и жди".
+//     Если светофор в аварийном режиме вывести "делай что хочешь"!
+
+
+
+
+
+// 1 task
+// порядку виводитись мають!!!!
+
+
+
+//
+// let one = prompt(1)
+// let two = prompt(2)
+// let three = prompt(3)
+// if () {
+//
+// }
+
+
+
+
+
+
+// 2 task
+// let color = prompt('Color');
+//
+// if (color === 'green') {
+//     document.write('GO')
+//
+// } else if (color === 'yellow') {
+//     console.log ('wait')
+//
+// } else if (color = 'red') {
+//     console.log('stop')
+//
+// } else (color = 'defective') {
+//     console.log('doo what you wont')
+// };
+
+
+
+// 3 task
+//     let isRoadClear
+// if (Если светофор зеленый и машин нет) {
+//    document.write(Go)
+// }
+// else if (Если светофор зеленый и машины есть) {
+//     console.log(wait go away)
+// }
+//
+// else if (Если светофор желтый и машины есть) {
+// console.log(wait)
+// }
+//
+// else if () {
+//
+// }
+
+
+    //test
+
+
+//
+// let users = [
+//     new User('vasya', 31, true),
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+//
+// function User (name,age,status) {
+//     this.name = name;
+//     this.age = age;
+//     this.status = status;
+//     this.greeting = function (msg) {
+//         console.log(`${msg} my name ${this.name} `)
+//     }
+// }
+// let user = new User('kokos', 123, false);
+// console.log(User);
+// user.greeting('hi!');
+//
+//
+// function UserWithWife(name = 'john', age = 30, wife = {name: 'tina', age: 29}) {
+//     this.name = name;
+//     this.age = age;
+//     this.wife = wife;
+// }
+//
+// let userWithWife = new UserWithWife('vasya', 31, {name: 'oly', age: 34});
+// console.log(userWithWife);
+//
+// function UserWithWifeAgreg(name, age, wName, wAge) {
+//     this.name = name;
+//     this.age = age;
+//     this.wife = {name: wName, age: wAge};}
+//
+// let userWithWifeAgreg = new UserWithWifeAgreg('asd', 123, 'qwe', 234);
+// console.log(userWithWifeAgreg);
+
+
+
+// let a = 0
+// if (a === 0) {
+//     console.log('virno')
+//
+// } else
+//     {
+//         console.log("ne virno")
+//     }
+
+
+
+// let min = 15;
+// if (min > 0 && min < 15) {
+//     console.log('1');
+// }
+//
+//
+// else if (min > 16 && min < 30) {
+//     console.log('2');
+// }
+//
+// else if (min > 31 && min < 45) {
+//     console.log('3');
+// }
+//
+// else if (min > 46 && min < 59)
+// {
+//     console.log('4');
+// }
+
+
+// let season = 3;
+//
+// switch (season) {
+//     case 1:
+//         console.log('winter'); break;
+//     case 2:
+//         console.log('autumn'); break;
+//     case 3:
+//         console.log('spring'); break;
+//     case 4:
+//         console.log('summer');
+// }
