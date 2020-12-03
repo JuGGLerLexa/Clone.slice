91cfa791f81f2bdc050f735ebd5b1bb81fb4efe1
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2020-12-03 20:11:21 +0200

    drgd

---
+++ leson1/src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Users from "./usersDZ/users";
+
+class App extends Component {
+    user = [
+        {name: "Max", age: 19, status: false},
+        {name: "Lexa", age: 20, status: true},
+        {name: "Egor", age: 21, status: true},
+        {name: "Vlad", age: 20, status: false},
+        {name: "Dima", age: 20, status: true},
+        {name: "Kolya", age: 20, status: true},
+        {name: "Roma", age: 24, status: false},
+        {name: "Yra", age: 23, status: true},
+        {name: "Pasha", age: 23, status: false},
+        {name: "Ananas", age: 123, status: true},
+    ];
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
