ee4ecc1661e45573c79d85f5265e71242dda46de
JuGGLerLexa <71986453+JuGGLerLexa@users.noreply.github.com>
2021-04-16 22:16:07 +0300

    Start test pure CSS (SCSS)

--- leson1/src/App.js
+++ leson1/src/App.js
@@ -2,27 +2,52 @@ import React, {Component} from 'react';
 import Users from "./usersDZ/users";
 
 class App extends Component {
-    user = [
-        {name: "Max", age: 19, status: false},
-        {name: "Lexa", age: 20, status: true},
-        {name: "Egor", age: 21, status: true},
-        {name: "Vlad", age: 20, status: false},
-        {name: "Dima", age: 20, status: true},
-        {name: "Kolya", age: 20, status: true},
-        {name: "Roma", age: 24, status: false},
-        {name: "Yra", age: 23, status: true},
-        {name: "Pasha", age: 23, status: false},
-        {name: "Ananas", age: 123, status: true},
+
+    man = [
+        {id: 12, name: "Max", age: 19, wife_id: false},
+        {id: 13, name: "Lexa", age: 20, wife_id: true},
+        {id: 14, name: "Egor", age: 21, wife_id: true},
+        {id: 15, name: "Vlad", age: 20, wife_id: false},
+        {id: 16, name: "Dima", age: 20, wife_id: true},
+        {id: 17, name: "Kolya", age: 20, wife_id: true},
+        {id: 18, name: "Roma", age: 24, wife_id: false},
+        {id: 19, name: "Yra", age: 23, wife_id: true},
+        {id: 20, name: "Pasha", age: 23, wife_id: false},
+        {id: 21, name: "Ananas", age: 123, wife_id: true},
     ];
+
+
+    woman = [
+        {id: 12, name: "Vika", age: 19, husband_id: false},
+        {id: 12, name: "Olya", age: 20, husband_id: true},
+        {id: 12, name: "Ira", age: 21, husband_id: true},
+        {id: 12, name: "Katya", age: 20, husband_id: false},
+        {id: 12, name: "Nastya", age: 20, husband_id: true},
+        {id: 12, name: "Santa", age: 20, husband_id: true},
+        {id: 12, name: "Ylya", age: 24, husband_id: false},
+        {id: 12, name: "Erika", age: 23, husband_id: true},
+        {id: 12, name: "Zzz", age: 23, husband_id: false},
+        {id: 12, name: "Xxx", age: 123, husband_id: true},
+    ];
+
+
+
 
 
     render() {
         return (
             <div>
-
+                this.man
             </div>
         );
     }
 }
+
+
+
+
+
+
+
 
 export default App;
